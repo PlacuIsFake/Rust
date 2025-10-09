c4665ceb4aa6d5cecab7c26c651ae5b450e646de
PlacuIsFake <pastefanel@gmail.com>
2025-10-09 15:36:49 +0300

    lab2 ex1

---
+++ lab2/ex1/src/main.rs
@@ -0,0 +1,20 @@
+fn add_chars_n(mut s: String, ch: char, n: i32) -> String {
+    let mut i = 0;
+    while i < n {
+        s.push(ch);
+        i = i + 1;
+    }
+    return s;
+}
+fn main() {
+    let mut s = String::from("");
+    let mut i = 0;
+    while i < 26 {
+        let c = (i as u8 + b'a') as char;
+        s = add_chars_n(s, c, 26 - i);
+
+        i += 1;
+    }
+
+    print!("{}", s);
+}
