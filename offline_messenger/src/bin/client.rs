13fec3f4b27df27f08542ef9ceda8cadd3bc2c11
PlacuIsFake <pastefanel@gmail.com>
2025-12-14 19:42:33 +0200

    axum pentru comunicarea dintre server si client

--- offline_messenger/src/bin/client.rs
+++ offline_messenger/src/bin/client.rs
@@ -1,3 +1,47 @@
-fn main() {
-    println!("Hello, world!");
+use serde::{Deserialize, Serialize};
+use std::error::Error;
+
+#[derive(Serialize, Debug)]
+struct CreateUser {
+    username: String,
+    password: String,
+}
+#[derive(Deserialize, Debug)]
+struct UserResponse {
+    id: u64,
+    username: String,
+    password: String,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let client = reqwest::Client::new();
+    let base_url = "http://127.0.0.1:3000";
+
+    let resp = client
+        .get(format!("{base_url}/"))
+        .send()
+        .await?
+        .text()
+        .await?;
+    println!("{resp}");
+
+    println!("\n--- 2. Testing POST /users ---");
+    let new_user = CreateUser {
+        username: "Pax".to_string(),
+        password: "password".to_string(),
+    };
+    let user_resp = client
+        .post(format!("{base_url}/users"))
+        .json(&new_user)
+        .send()
+        .await?
+        .json::<UserResponse>()
+        .await?;
+    println!(
+        "{}, {}, {}",
+        user_resp.id, user_resp.username, user_resp.password
+    );
+    println!("Create User: {user_resp:?}");
+    Ok(())
 }
--- offline_messenger/src/bin/server.rs
+++ offline_messenger/src/bin/server.rs
@@ -1,3 +1,65 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    Json, Router,
+    http::StatusCode,
+    response::IntoResponse,
+    routing::{get, post},
+};
+use serde::{Deserialize, Serialize};
+use std::error::Error;
+
+#[derive(Deserialize)]
+struct CreateUser {
+    username: String,
+    password: String,
+}
+#[derive(Serialize)]
+struct User {
+    id: u64,
+    username: String,
+    password: String,
+}
+#[derive(Deserialize)]
+struct NewMessage {
+    from: u64,
+    to: u64,
+    message: String,
+}
+#[derive(Serialize)]
+struct Message {
+    id: u64,
+    from: u64,
+    to: u64,
+    message: String,
+}
+async fn root() -> &'static str {
+    "Welcome to offline_messenger!"
+}
+async fn create_user(Json(payload): Json<CreateUser>) -> impl IntoResponse {
+    let user = User {
+        id: 1984,
+        username: payload.username,
+        password: payload.password,
+    };
+    (StatusCode::CREATED, Json(user))
+}
+async fn new_message(Json(payload): Json<NewMessage>) -> impl IntoResponse {
+    let mes = Message {
+        id: 1,
+        from: payload.from,
+        to: payload.to,
+        message: payload.message,
+    };
+    (StatusCode::CREATED, Json(mes))
+}
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    tracing_subscriber::fmt::init();
+    let app = Router::new()
+        .route("/", get(root))
+        .route("/users", post(create_user))
+        .route("/messages", post(new_message));
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await?;
+    tracing::debug!("listening on {}", listener.local_addr()?);
+    axum::serve(listener, app).await?;
+    Ok(())
 }
