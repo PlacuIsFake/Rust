f4610d2382eba7a92be3c683e87a9fc31c41da58
PlacuIsFake <pastefanel@gmail.com>
2026-01-08 13:14:31 +0200

    Eroarea nu mai este returnata din main, este verificata intr-un match

--- offline_messenger/server/src/main.rs
+++ offline_messenger/server/src/main.rs
@@ -1,11 +1,15 @@
 mod network_manager;
-use std::error::Error;
 
 use network_manager::server::Server;
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
+async fn main() {
     let server = Server::new();
-    server.start().await?;
-    Ok(())
+    match server.start().await {
+        Ok(_) => {},
+        Err(err) => {
+            println!("Error while starting the server: {err}");
+        },
+    }
+    
 }
